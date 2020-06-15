051111e5074c7981042208c6feab564601c82a0a
Tran Thi Tuyet Ngan <ngan.tran21@pn.com.vn>
2020-06-15 08:55:44 +0700

    Add create table and seeder

---
+++ database/seeds/RoomSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use Illuminate\Support\Facades\DB;
+
+class RoomSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        for($i = 0; $i<10; $i++){
+            DB::table('rooms')->insert([
+                'name' => $faker->catchPhrase,
+                'image' => $faker->imageUrl($width = 640, $height = 480),
+                'typeroom' => $faker->randomNumber(1),
+                'number' => $faker->randomNumber(2),
+                'area' => $faker->randomNumber(2),
+                'price' => $faker->randomNumber(6),
+            ]);
+        }
+    }
+}
